95ed68d2119c0a68cfcf2dbdf86ca560b88e200a
DestinyMatuse <lillymatuse@gmail.com>
2023-09-15 23:46:56 -0700

    update

--- custom_getline.c
+++ custom_getline.c
@@ -1,62 +1,78 @@
 #include "main.h"
+#include <errno.h>
+
 /**
- * custom_getline - my custome getline function
- * @lineptr: the buffer to hold the stream input
- * @n: the size of the buffer
- * @stream: the stream where the input will be made
+ * custom_getline - Custom getline function for reading lines from a stream.
+ * @lineptr: Pointer to the buffer to hold the line.
+ * @n: Pointer to the size of the buffer.
+ * @stream: The stream where the input will be read from.
  *
- * Return: the total characters read. -1 if it fails
+ * Return: The total characters read, or -1 if it fails.
  */
-ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream) {
+ssize_t custom_getline(char **lineptr, size_t *n, FILE *stream)
+{
+	size_t bufsize = 0;
+	size_t chars_read = 0;
+	char *temp;
 
-    size_t bufsize = 0;
-    size_t chars_read;
-    int errno;
-    char *temp;
+	/* Check for invalid arguments */
+	if (lineptr == NULL || n == NULL)
+	{
+		errno = EINVAL;
+		return (-1);
+	}
 
-    if (lineptr == NULL || n == NULL) {
-        errno = EINVAL;
-        return -1;
-    }
+	/* If the buffer is not initialized, allocate memory */
+	if (*lineptr == NULL || *n == 0)
+	{
+		bufsize = BUF_SIZE; /* Initial buffer size (can be adjusted) */
+		*lineptr = (char *)malloc(bufsize);
+		if (*lineptr == NULL)
+		{
+			return (-1); /* Memory allocation error */
+		}
+		*n = bufsize; /* Ownership of memory passed to the caller */
+	}
 
-    /* Allocate memory for the line buffer if needed*/
-    if (*lineptr == NULL || *n == 0) {
-        bufsize = BUF_SIZE;  /*Initial buffer size (can be adjusted)*/
-        *lineptr = (char *)malloc(bufsize);
-        if (*lineptr == NULL) {
-            return -1;
-        }
-        *n = bufsize;
-    }
+	/* Read characters until a newline or EOF is encountered */
 
-    /*Read characters until a newline or EOF is encountered*/
-    chars_read = 0;
-    while (1) {
-        int c = fgetc(stream);
+	/* This code has too many functions inside the nested loop
+	 * I have fixed betty style and yet i keep on running across too many function errors
+	 *  please review the code again, is it possible that we can use few if statement and 
+	 *  run while loop without using too much if statement
+	 * */
+	while (1)
+	{
+		int c = fgetc(stream);
 
-        if (c == EOF || c == '\n') {
-            if (chars_read == 0 && c == EOF) {
-                return -1; 
-            }
-            (*lineptr)[chars_read] = '\0';  /* Null-terminate the line*/
-            break;
-        }
+		if (c == EOF || c == '\n')
+		{
+			if (chars_read == 0 && c == EOF)
+			{
+				return (-1); /* No characters read, and EOF reached */
+			}
+			(*lineptr)[chars_read] = '\0'; /* Null-terminate the line */
+			break;
+		}
+		(*lineptr)[chars_read] = (char)c;
+		chars_read++;
 
-        (*lineptr)[chars_read] = (char)c;
-        chars_read++;
-
-        /* Resize the buffer if needed*/
-        if (chars_read >= bufsize - 1) {
-            bufsize *= 2;
-            temp = (char *)realloc(*lineptr, bufsize);
-            if (temp == NULL) {
-                return -1;
-            }
-            *lineptr = temp;
-            *n = bufsize;
-        }
-    }
-
-    return chars_read;
+		/* Resize the buffer if needed */
+		if (chars_read >= bufsize - 1)
+		{
+			bufsize *= 2;
+			temp = (char *)realloc(*lineptr, bufsize);
+			if (temp == NULL)
+			{
+				/* Memory allocation error during realloc */
+				free(*lineptr); /* Clean up previously allocated memory */
+				*lineptr = NULL; /* Prevent dangling pointer */
+				return (-1);
+			}
+			*lineptr = temp;
+			*n = bufsize;
+		}
+	}
+	return (chars_read);
 }
 
--- path_dir.c
+++ path_dir.c
@@ -1,42 +1,48 @@
 #include "main.h"
+
 /**
  * dir_path - displays the path directory
  * @path_dir: the directory of the path
- * @usr_command: Command entered by user
+ * @usr_command: Command entered by the user
  *
- * Return: NULL
+ * Return: NULL if the file is not found; otherwise, the full path of the file
  */
 char *dir_path(char *path_dir, char *usr_command)
 {
-	char *path_token, *delim, *command_cp;
-	struct stat status;
-	int com_len;
-	char *file_path;
+char *path_token, *delim, *command_cp;
+struct stat status;
+int com_len;
+char *file_path;
 
-	delim = ":";
-	com_len = _strlen(usr_command);
-	/*making a copy of the whole PATH*/
-	command_cp = _strdup(path_dir);
-	path_token = strtok(command_cp, delim);
-	if (path_token != NULL)
-		 /*locating memory for the path*/
-	 file_path = malloc(com_len + _strlen(path_token) + 2);
-	while (path_token != NULL)
-	{
-		 
+delim = ":";
+com_len = _strlen(usr_command);
 
-		_strcpy(file_path, path_token);
-		_strcat(file_path, "/");
-		_strcat(file_path, usr_command);
-		if (stat(file_path, &status) == 0)
-		{
-			free(command_cp);
-			return (file_path);
-		}
-		else{
-			path_token = strtok(NULL, delim);
-		}
+/* Making a copy of the whole PATH */
+command_cp = _strdup(path_dir);
+path_token = strtok(command_cp, delim);
 
-	}
-	return (NULL);
+if (path_token != NULL)
+{
+/* Allocate memory for the path */
+file_path = malloc(com_len + _strlen(path_token) + 2);
 }
+
+while (path_token != NULL)
+{
+_strcpy(file_path, path_token);
+_strcat(file_path, "/");
+_strcat(file_path, usr_command);
+
+if (stat(file_path, &status) == 0)
+{
+free(command_cp);
+return (file_path);
+}
+else
+{
+path_token = strtok(NULL, delim);
+}
+}
+return (NULL);
+}
+
